fbf9e8ed707d2a7caf72237f58b049da7385c9ff
habeshawit <shalomseleshi@gmail.com>
2021-03-20 09:18:27 -0400

    post request initial setup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const endPoint = "http://localhost:3000/api/v1/books";
 
 document.addEventListener("DOMContentLoaded", () => {
   getBooks();
+
+  const createBookForm = document.querySelector("#create-book-form");
+
+  createBookForm.addEventListener("submit", (e) => createFormHandler(e));
 });
 
 function getBooks() {
@@ -24,4 +28,37 @@ function getBooks() {
         document.querySelector("#book-container").innerHTML += bookMarkup;
       });
     });
+}
+
+function createFormHandler(e) {
+  e.preventDefault();
+  const title = document.querySelector("#input-title").value;
+  const author = document.querySelector("#input-author").value;
+  const price = document.querySelector("#input-price").value;
+  const description = document.querySelector("#input-description").value;
+  const seller_info = document.querySelector("#input-seller_info").value;
+  const image = document.querySelector("#input-url").value;
+  const categoryId = parseInt(document.querySelector("#categories").value);
+  // const categoryId = parseInt(category); //parse into integer
+  postBook(title, author, price, description, seller_info, image, categoryId);
+}
+
+function postBook(
+  title,
+  author,
+  price,
+  description,
+  seller_info,
+  image,
+  categoryId
+) {
+  console.log(
+    title,
+    author,
+    price,
+    description,
+    seller_info,
+    image,
+    categoryId
+  );
 }
