1b8385de31e7a9c6b3f26063a25e56f38ce17316
habeshawit <shalomseleshi@gmail.com>
2021-03-23 13:47:02 -0400

    book title added to popup

--- src/book.js
+++ src/book.js
@@ -155,7 +155,7 @@ class Book {
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">${this.title}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
